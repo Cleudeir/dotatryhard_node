2264b2daaa0228a15b82239741a5d140bbb973c7
Cleudeir <cleudeirsilva@gmail.com>
2022-08-30 13:54:35 -0300

    refactory

---
+++ src/class/Steam.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+class Steam {
+
+    public constructor() {
+   
+    }
+
+}
+export default new Steam()
--- src/components/Steam/matchHistory.js
+++ src/components/Steam/matchHistory.js
@@ -9,7 +9,23 @@ export default async function matchHistory(accountId, length) {
         const request = await fetch(`${process.env.base_url}IDOTA2Match_570/GetMatchHistory/v1/?account_id=${accountId}&game_mode=${process.env.game_mode}&key=${process.env.key_api}`)
         const data = await request.json()
         if (data && data.result && data.result.matches) {
-            return data.result.matches.splice(0, length);
+            //Matchs
+            const matchesSingle = new Set();
+            const playersSingle = new Set();
+            data.result.matches.forEach((match) => {
+                matchesSingle.add(match.match_id)
+                match.players.forEach((player) => {
+                    if (+player.account_id === 4294967295) {
+                        playersSingle.add(player.player_slot + 1);
+                    } else {
+                        playersSingle.add(player.account_id);
+                    }
+                },
+                );
+            });
+            const matches = ([...matchesSingle]).filter((x) => x > null);
+            const players = ([...playersSingle]).filter((x) => x > 0);
+            return { matches, players };
         }
         console.log('matchHistory', data)
         return null;
