9cb28ea41dc4e3e398a1c69c7c824abd48c5d9cf
Cleudeir <cleudeirsilva@gmail.com>
2022-11-16 18:08:09 -0300

    update profile

--- src/index.ts
+++ src/index.ts
@@ -67,12 +67,17 @@ const _ranking = new Revalidate("ranking", 4)
 
 let count: number = 0
 let inUse = false
-
+let data = Date.now()
 server.get('/ranking', async (req, res) => {
     const time = Date.now()
     const limit = req.query.limit as unknown as string
     console.log({ limit })
     let result = await _ranking.check(ranking, +limit)
+    if(Date.now() - data > 1000*60*60*24 ){ 
+        count = 0
+        inUse = false
+        data = Date.now()
+    }
     if (inUse === false) {        
         const updateData : any = async function () {
             const element = result[count];
@@ -83,10 +88,7 @@ server.get('/ranking', async (req, res) => {
                 await updateMatches(element.profile.account_id)
                 if (count < result.length-1) {                
                     updateData()
-                } else {
-                    count = 0
-                    inUse = false
-                }
+                } 
             }           
         }   
         updateData()    
