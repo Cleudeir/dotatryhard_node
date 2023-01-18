70e247759f054dbfb5eb4ca2490b55fdd2b76382
Cleudeir <cleudeirsilva@gmail.com>
2023-01-18 15:30:48 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -63,12 +63,12 @@ avgGlobalCache.check(avgGlobal).then((_avgGlobal) => {
         async function createCacheInfos() {
             const account_id: number = Number(result[count].profile.account_id)
             limit = 1000
-            const _infos: any = new Revalidate('infos_' + account_id, 18 * 60 + Math.floor(Math.random() * 6))
+            const _infos: any = new Revalidate('infos_' + account_id, 23 * 60 + Math.floor(Math.random() * 1))
             console.log({ count, account_id, limit })
             await start(account_id)
             await _infos.check(infos, { account_id, limit })
             if (count < result.length) {
-                await sleep(60 * 1000)
+                await sleep(60 * 1400)
                 createCacheInfos()
                 count += 1
             }
